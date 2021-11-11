286df5451722cea60612740ff79e6daa205b4982
Stephenfre <stephen.freeman.dev@gmail.com>
2021-11-11 00:56:38 -0700

    ADDED: Created array of cities for search filter

--- app/components/MyLocations.js
+++ app/components/MyLocations.js
@@ -9,103 +9,65 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const data = {
-    currentTemp: 88,
-    hiTemp: 92,
-    lowTemp: 66,
-    currentCondition: "Partly Cloudy",
-    currentCity: "Phoenix",
-    currentState: "AZ",
-    currentWind: 12,
-    precipitation: "60%",
-    hourlyForcast: [
+    locations: [
         {
             id: 1,
-            time: "7am",
-            condition: "partly cloudy",
-            temp: 74,
+            city: "Phoenix, AZ",
+            country: "USA",
         },
+
         {
             id: 2,
-            time: "8am",
-            condition: "partly cloudy",
-            temp: 79,
+            city: "New York, NY",
+            country: "USA",
         },
         {
             id: 3,
-            time: "9am",
-            condition: "partly cloudy",
-            temp: 83,
+            city: "Denver, CO",
+            country: "USA",
         },
+
         {
             id: 4,
-            time: "10am",
-            condition: "partly cloudy",
-            temp: 86,
+            city: "Chicago, IL",
+            country: "USA",
         },
         {
             id: 5,
-            time: "11am",
-            condition: "partly cloudy",
-            temp: 88,
+            city: "Austin, TX",
+            country: "USA",
         },
+
         {
             id: 6,
-            time: "12pm",
-            condition: "partly cloudy",
-            temp: 91,
+            city: "San Deigo, CA",
+            country: "USA",
         },
         {
             id: 7,
-            time: "1pm",
-            condition: "partly cloudy",
-            temp: 94,
+            city: "Detroit, MI",
+            country: "USA",
         },
+
         {
             id: 8,
-            time: "2pm",
-            condition: "partly cloudy",
-            temp: 96,
-        },
-    ],
-    weeklyForcast: [
-        {
-            id: 1,
-            day: "Monday",
-            hiTemp: 85,
-            lowTemp: 68,
-        },
-        {
-            id: 2,
-            day: "Tuesday",
-            hiTemp: 90,
-            lowTemp: 70,
-        },
-        {
-            id: 3,
-            day: "Wednesday",
-            hiTemp: 87,
-            lowTemp: 68,
-        },
-        {
-            id: 4,
-            day: "Thursday",
-            hiTemp: 79,
-            lowTemp: 65,
-        },
-        {
-            id: 5,
-            day: "Friday",
-            hiTemp: 80,
-            lowTemp: 66,
-        },
-        {
-            id: 6,
-            day: "Saturday",
-            hiTemp: 75,
-            lowTemp: 62,
-        },
-    ],
 
+            city: "Portland, OR",
+            country: "USA",
+        },
+        {
+            id: 9,
+
+            city: "Seattle, WA",
+            country: "USA",
+        },
+
+        {
+            id: 10,
+            city: "Vancouver, WA",
+            country: "Canada",
+        },
+    ],
     savedLocations: [
         {
             id: 1,

833927050ef820c53b90224b8a0e9a9279b58752
Stephenfre <stephen.freeman.dev@gmail.com>
2021-11-23 20:14:26 -0700

    fixed key warning

--- app/components/HourlyForecast.js
+++ app/components/HourlyForecast.js
@@ -1,15 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { View, SafeAreaView, ScrollView, Dimensions, Text, Image } from "react-native";
-// import { getWeatherData } from "../store/actions";
-// import { connect } from "react-redux";
-import { useDispatch } from "react-redux";
+import React from "react";
+import { View, SafeAreaView, ScrollView, Text, Image } from "react-native";
 
-import axios from "axios";
 import moment from "moment";
 
 import styles from "../assets/style/HourlyStyles";
-
-const { width } = Dimensions.get("window");
 
 export default function HourlyForecast({ weatherData }) {
     const now = moment().format("ha");
@@ -20,19 +14,13 @@ export default function HourlyForecast({ weatherData }) {
             <Text>loading...</Text>
         </SafeAreaView>
     ) : (
-        <ScrollView
-            horizontal={true}
-            decelerationRate={0}
-            // snapToInterval={width - 60}
-            // snapToAlignment={"center"}
-            showsHorizontalScrollIndicator={false}
-        >
-            {weatherData.hourly.map((hour) => {
+        <ScrollView horizontal={true} decelerationRate={0} showsHorizontalScrollIndicator={false}>
+            {weatherData.hourly.map((hour, index) => {
                 // console.log("hourly", moment(hour.dt * 1000).format("ha") === now);
                 // console.log("hourly", moment(hour.dt * 1000).format("ha") === moment().format("h"));
                 // console.log("hourly", moment().format("ha"));
                 return (
-                    <View style={styles.middleContent}>
+                    <View key={index} style={styles.middleContent}>
                         {hour.pop > 0 ? (
                             <>
                                 <Text style={styles.rainTime}>

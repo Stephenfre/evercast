abfe6d89f030f34c0c837c236df0bc759fb74f25
Stephen Freeman <stephen.freeman.dev@gmail.com>
2021-11-24 20:34:51 -0700

    Removed weather cards from MyLocations.js and added them to WeatherCards.js

--- app/components/MyLocations.js
+++ app/components/MyLocations.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import axios from "axios";
 import { connect } from "react-redux";
 import {
     Text,
@@ -12,101 +11,18 @@ import {
     Modal,
     Alert,
     Dimensions,
-    Image,
 } from "react-native";
 
 import NewLocation from "./NewLocation";
+import WeatherCards from "./WeatherCards";
 
 import styles from "../assets/style/MyLocationsStyles";
-import currentStyles from "../assets/style/CurrentStyles";
-
-import { LinearGradient } from "expo-linear-gradient";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 function SavedLocations({ savedLocations }) {
     const [query, setQuery] = useState("");
     const [city, setCity] = useState([]);
     const [modalVisible, setModalVisible] = useState(false);
     const focusSearch = useRef(null);
-    const [data, setData] = useState({
-        locations: [
-            {
-                id: 1,
-                city: "Phoenix, AZ",
-                country: "USA",
-            },
-
-            {
-                id: 2,
-                city: "New York, NY",
-                country: "USA",
-            },
-            {
-                id: 3,
-                city: "Denver, CO",
-                country: "USA",
-            },
-
-            {
-                id: 4,
-                city: "Chicago, IL",
-                country: "USA",
-            },
-            {
-                id: 5,
-                city: "Austin, TX",
-                country: "USA",
-            },
-
-            {
-                id: 6,
-                city: "San Deigo, CA",
-                country: "USA",
-            },
-            {
-                id: 7,
-                city: "Detroit, MI",
-                country: "USA",
-            },
-
-            {
-                id: 8,
-                city: "Portland, OR",
-                country: "USA",
-            },
-            {
-                id: 9,
-                city: "Seattle, WA",
-                country: "USA",
-            },
-
-            {
-                id: 10,
-                city: "Vancouver, WA",
-                country: "Canada",
-            },
-        ],
-        savedLocations: [
-            {
-                id: 1,
-                temp: 88,
-                city: "Phoenix",
-                country: "USA",
-                wind: 12,
-                precip: "60%",
-            },
-
-            {
-                id: 2,
-                temp: 66,
-                city: "New York",
-                country: "USA",
-                wind: 20,
-                precip: "60%",
-            },
-        ],
-    });
 
     const { width, height } = Dimensions.get("window");
 
@@ -262,80 +178,7 @@ function SavedLocations({ savedLocations }) {
                     </SafeAreaView>
                 ) : null}
 
-                {/* 
-                    SAVED WEATHERS CARDS ARE BELOW
-                */}
-
-                <View style={styles.locations}>
-                    {savedLocations.map((res, index) => {
-                        return (
-                            <View key={index}>
-                                <View style={res.temp > 66 ? styles.locationCardA : styles.locationCardB}>
-                                    {res.temp > 66 ? (
-                                        <LinearGradient
-                                            start={{ x: 1, y: 0 }}
-                                            end={{ x: 0, y: 1 }}
-                                            colors={["rgb(255,184,0)", "transparent"]}
-                                            style={styles.cardBackground}
-                                        />
-                                    ) : (
-                                        <LinearGradient
-                                            start={{ x: 1, y: 0 }}
-                                            end={{ x: 0, y: 1 }}
-                                            colors={["rgb(162,216,247)", "transparent"]}
-                                            style={styles.cardBackground}
-                                        />
-                                    )}
-
-                                    <View style={styles.topCard}>
-                                        <View style={styles.savedCity}>
-                                            <Text style={styles.locationTemp}>{Math.round(res.temp)}</Text>
-                                            <Text style={styles.locationCity}>{res.city}</Text>
-                                            <Text style={styles.locationCountry}>{res.country}</Text>
-                                        </View>
-
-                                        <View>
-                                            <Image
-                                                source={{
-                                                    uri: "http://openweathermap.org/img/wn/" + res.img + "@4x.png",
-                                                }}
-                                                style={{ width: 60, height: 80 }}
-                                            />
-                                        </View>
-                                    </View>
-                                    <View style={styles.locationDetails}>
-                                        <View
-                                            style={{
-                                                justifyContent: "center",
-                                                alignItems: "center",
-                                                flexDirection: "row",
-                                                paddingRight: 40,
-                                            }}
-                                        >
-                                            <Ionicons name="ios-rainy" color="white" size={13} />
-                                            <Text style={styles.savedDetails}>{res.precip}</Text>
-                                        </View>
-                                        <View
-                                            style={{
-                                                justifyContent: "center",
-                                                alignItems: "center",
-                                                flexDirection: "row",
-                                                paddingLeft: 5,
-                                            }}
-                                        >
-                                            <MaterialCommunityIcons
-                                                name="weather-windy-variant"
-                                                color="white"
-                                                size={13}
-                                            />
-                                            <Text style={styles.savedDetails}>{res.wind} kh/m</Text>
-                                        </View>
-                                    </View>
-                                </View>
-                            </View>
-                        );
-                    })}
-                </View>
+                <WeatherCards />
             </View>
         </ScrollView>
     );

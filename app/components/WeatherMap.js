1bd608511d2d33c43e2f23054effa5a36f7983e8
Stephen Freeman <stephen.freeman.dev@gmail.com>
2021-11-29 17:29:23 -0700

    added location and location marker

--- app/components/WeatherMap.js
+++ app/components/WeatherMap.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Dimensions, Text, SafeAreaView } from "react-native";
 
 import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 function WeatherMap(props) {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    console.log("location", location);
+
+    const myLocation = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    };
+
+    let text = "Waiting..";
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
     return (
         <SafeAreaView style={styles.container}>
-            <MapView
-                initialRegion={{
-                    latitude: 33.50271,
-                    longitude: -112.0484,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-                style={styles.map}
-            />
+            <MapView initialRegion={myLocation} provider={MapView.PROVIDER_GOOGLE} style={styles.map}>
+                <Marker coordinate={myLocation} />
+            </MapView>
         </SafeAreaView>
     );
 }

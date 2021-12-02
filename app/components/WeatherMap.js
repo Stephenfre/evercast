e674a9cea8c5c9473adb1d454066d7fb51d173cb
Stephen Freeman <stephen.freeman.dev@gmail.com>
2021-12-02 16:20:34 -0700

    small changes

--- app/components/WeatherMap.js
+++ app/components/WeatherMap.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Dimensions, Text, SafeAreaView } from "react-native";
 
 import MapView from "react-native-maps";
-import { Marker } from "react-native-maps";
+import { Marker, UrlTile } from "react-native-maps";
 import * as Location from "expo-location";
 import axios from "axios";
 
@@ -11,43 +11,51 @@ function WeatherMap(props) {
     const [errorMsg, setErrorMsg] = useState(null);
     const [mapData, setMapData] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== "granted") {
-                setErrorMsg("Permission to access location was denied");
-                return;
-            }
+    const Title =
+        "https://tile.openweathermap.org/map/{precipitation_new}/{3}/{2}/{4}.png?appid=7613dff2af161bb376b90a08f6c9d4df";
 
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-        })();
-    }, []);
+    // useEffect(() => {
+    //     (async () => {
+    //         let { status } = await Location.requestForegroundPermissionsAsync();
+    //         if (status !== "granted") {
+    //             setErrorMsg("Permission to access location was denied");
+    //             return;
+    //         }
 
-    console.log("location", location);
+    //         let location = await Location.getCurrentPositionAsync({});
+    //         setLocation(location);
+    //     })();
+    // }, []);
 
-    let text = "Waiting..";
-    if (errorMsg) {
-        text = errorMsg;
-    } else if (location) {
-        text = JSON.stringify(location);
-    }
+    // console.log("location", location);
+
+    // let text = "Waiting..";
+    // if (errorMsg) {
+    //     text = errorMsg;
+    // } else if (location) {
+    //     text = JSON.stringify(location);
+    // }
+
     return (
         <SafeAreaView style={styles.container}>
             <MapView
                 initialRegion={{
-                    latitude: location.coords.latitude,
-                    longitude: location.coords.longitude,
+                    latitude: 40.71,
+                    longitude: -74.0,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
+                maximumZ={6}
+                zIndex={1}
+                tileSize={256}
+                UrlTile="https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=7613dff2af161bb376b90a08f6c9d4df"
                 provider={MapView.PROVIDER_GOOGLE}
                 style={styles.map}
             >
                 <Marker
                     coordinate={{
-                        latitude: location.coords.latitude,
-                        longitude: location.coords.longitude,
+                        latitude: 40.71,
+                        longitude: -74.0,
                         latitudeDelta: 0.0922,
                         longitudeDelta: 0.0421,
                     }}
